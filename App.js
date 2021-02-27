e2a875dccb0f55c61c1a1950652fae541e48e327
Olimjon <olimjon.nematov@gmail.com>
2021-02-27 14:49:36 -0600

    added react navigation

--- App.js
+++ App.js
@@ -2,36 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-//firbase
-import apiKeys from "./constants/apikeys";
+//navigation
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native'
+import MainNav from "./screens/mainNav"
+import HomeScreen  from "./screens/home"
 
-
-
-export default class App extends React.Component{
-
-  // constructor (props){
-  //   super(props);
-  //   this.state={isLoadingComplete: false};
-    
-  //   //initialize firbase
-  //   if(!firebase.apps.length){firebase.initializeApp(ApiKeys.firebaseConfig);}
-  // }
-  render(){
-    return (
-      <View style={styles.container}>
-        <Text>hello!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+function App (){
+  return(
+   <MainNav></MainNav>
+  );
   
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/mainNav.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen  from "./home";
+
+
+const Stack = createStackNavigator();
+
+function MainNav(){
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name = "home" component ={HomeScreen}/>
+                
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+    
+}
+
+export default MainNav;
