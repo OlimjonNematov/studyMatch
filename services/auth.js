749c93c98fe72ae2bc47915d950a0cbef393bd34
Olimjon <olimjon.nematov@gmail.com>
2021-02-27 17:26:40 -0600

    addded auth

---
+++ services/auth.js
@@ -0,0 +1,23 @@
+import auth from '@react-native-firebase/auth';
+
+export function createUser(email, password){
+    auth()
+  .createUserWithEmailAndPassword(email, password)
+  .then(() => {
+    console.log('User account created & signed in!');
+  })
+  .catch(error => {
+    if (error.code === 'auth/email-already-in-use') {
+      console.log('That email address is already in use!');
+    }
+
+    if (error.code === 'auth/invalid-email') {
+      console.log('That email address is invalid!');
+    }
+
+    console.error(error);
+  });
+}
+export function signIn(email, password){
+    auth().signInWithEmailAndPassword(email,password)
+}
