13d71a147a8d33caa018bf1104ccf81fbf61adb0
Olimjon <olimjon.nematov@gmail.com>
2021-02-27 13:00:24 -0600

    firbase installed

---
+++ constants/apikeys.js
@@ -0,0 +1,37 @@
+
+export default {
+        firebaseConfig:{
+            apiKey: "AIzaSyAIe6SG0tx0DRLe8bkh2rJd0rN731ZQBrs",
+            authDomain: "hackutd-c1131.firebaseapp.com",
+            projectId: "hackutd-c1131",
+            storageBucket: "hackutd-c1131.appspot.com",
+            messagingSenderId: "826599501394",
+            appId: "1:826599501394:web:a0a2f5b2d4824be3a7798b",
+            measurementId: "G-TBSCQLPXDJ"
+        }
+}
+/*
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAIe6SG0tx0DRLe8bkh2rJd0rN731ZQBrs",
+    authDomain: "hackutd-c1131.firebaseapp.com",
+    projectId: "hackutd-c1131",
+    storageBucket: "hackutd-c1131.appspot.com",
+    messagingSenderId: "826599501394",
+    appId: "1:826599501394:web:a0a2f5b2d4824be3a7798b",
+    measurementId: "G-TBSCQLPXDJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+*/ 
